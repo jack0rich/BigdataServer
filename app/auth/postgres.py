ae162836bf8427b9c6ba6604bceb047a30c9a909
Jack Rich <957715359@qq.com>
2025-01-30 14:49:21 +0800

    frame 0.2 (add auth)

--- app/auth/postgres.py
+++ app/auth/postgres.py
@@ -26,7 +26,7 @@ class User(Base):
     username = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
     api_key_encrypted = Column(BLOB, nullable=False)
-    created_at = Column(TIMESTAMP, default=datetime.now(timezone.utc))
+    created_at = Column(TIMESTAMP, default=datetime.now(timezone.utc))  
 
 
 Session = sessionmaker(bind=engine)
@@ -36,20 +36,22 @@ class Database:
     def __init__(self):
         self.session = Session()
 
-    def insert_user(self, username, password_hash, api_key_encrypted):
+    def insert_user(self, username, password_hash, api_key_encrypted) -> bool:
         try:
             user = User(username=username, password_hash=password_hash,
                          api_key_encrypted=api_key_encrypted)
             self.session.add(user)
             self.session.commit()
             logger.info(f"User {username} inserted successfully.")
+            return True
         except Exception as e:
             logger.error(f"Error inserting user: {e}")
             self.session.rollback()
+            return False
 
-    def fetch_user_by_id(self, user_id):
+    def fetch_user_by_name(self, user_name):
         try:
-            user = self.session.query(User).filter(User.id == user_id).first()
+            user = self.session.query(User).filter(User.username == user_name).first()
             return user
         except Exception as e:
             logger.error(f"Error fetching user by ID: {e}")
@@ -57,12 +59,12 @@ class Database:
         finally:
             self.session.close()
 
-    def update_user_password(self, user_id, new_password_hash) -> bool:
+    def update_user_password(self, username, new_password_hash) -> bool:
         """
         更新用户密码
         """
         try:
-            user = self.session.query(User).filter(User.id == user_id).first()
+            user = self.session.query(User).filter(User.username == username).first()
             if user:
                 user.password_hash = new_password_hash
                 self.session.commit()
@@ -75,36 +77,62 @@ class Database:
             logger.error(f"Error updating password: {e}")
             self.session.rollback()
 
-    def delete_user(self, user_id):
+    def delete_user(self, username) -> bool:
         """
         删除用户
         """
         try:
-            user = self.session.query(User).filter(User.id == user_id).first()
+            user = self.session.query(User).filter(User.username == username).first()
             if user:
                 self.session.delete(user)
                 self.session.commit()
                 logger.info(f"User {user.username} deleted.")
+                return True
             else:
                 logger.warning("User not found.")
+                return False
         except Exception as e:
             logger.error(f"Error deleting user: {e}")
             self.session.rollback()
+            return False
 
 
 
-def creat_user(username: str, password: str):
+def creat_user(username: str, password: str) -> bool:
     hashed_pw = u.hash_password(password)
     encrypt_api_key = u.encrypt_key(u.generate_secure_api_key())
     db = Database()
-    db.insert_user(
+    return db.insert_user(
         username=username,
         password_hash=hashed_pw,
         api_key_encrypted=encrypt_api_key
     )
 
 
+def update_user(username: str, password: str) -> bool:
+    hashed_pw = u.hash_password(password)
+    db = Database()
+    return  db.update_user_password(
+        username=username,
+        new_password_hash=hashed_pw
+    )
+
+
+def delete_user(username: str) -> bool:
+    db = Database()
+    return db.delete_user(
+        username=username
+    )
+
+
+def fetch_user_by_name(username: str):
+    db = Database()
+    return db.fetch_user_by_name(
+        user_name=username
+    )
+
 
 if __name__ == '__main__':
-    creat_user('jack', '0907')
+    update_user('jack', '9999')
+
 
